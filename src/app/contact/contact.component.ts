5268b8465eb6b0225cd1ec8f8285347b4018b9ba
skizotty <scottsorci@gmail.com>
2023-01-29 22:16:46 -0600

    Fixing issues w/ footer and add error handling. Also update Form Method to work with Nelify

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,28 +1,21 @@
-import { Component, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+import { Component, Inject } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"],
 })
 export class ContactComponent {
-  constructor(private http: HttpClient) {}
-
   form: FormGroup;
-  name = '';
-  phone = '';
-  email = '';
-  message = '';
+  name = "";
+  phone = "";
+  email = "";
+  message = "";
   isLoading = false;
   isSubmittedSuccessFully = false;
   isSubmitted = false;
+  errors = [];
   contactMe() {
     this.isSubmitted = true;
     this.isLoading = true;
@@ -31,15 +24,45 @@ export class ContactComponent {
       phone: this.phone,
       email: this.email,
       message: this.message,
-      success: true
+      success: true,
     };
-    this.http
-      .post('http://scottsorci.com/form_endpoint.php', contact)
-      .subscribe(res => {
+    // this.http.post("/", contact).subscribe((res) => {
+    //   this.isLoading = false;
+    //   this.isSubmittedSuccessFully = true;
+    //   console.log(res);
+    // });
+
+    let formData = new FormData();
+    formData.append("name", contact.name);
+    formData.append("phone", contact.phone);
+    formData.append("email", contact.email);
+    formData.append("message", contact.message);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      // @ts-ignore`
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          this.isSubmittedSuccessFully = false;
+          this.errors = [
+            "Something went wrong... Give a few minutes than try again!",
+          ];
+        } else {
+          this.isSubmittedSuccessFully = true;
+        }
         this.isLoading = false;
-        this.isSubmittedSuccessFully = true;
         console.log(res);
+      })
+      .catch((error) => {
+        alert(error);
+        this.isSubmittedSuccessFully = false;
+        this.errors = [
+          "Something went wrong... Give a few minutes than try again!",
+        ];
       });
-    // console.log(contact);
   }
+  // console.log(contact);
 }
